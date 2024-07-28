91420aef8c5412250e5007c497244c8888e3f221
tarkex0x <163349630+tarkex0x@users.noreply.github.com>
2024-07-28 16:04:29 +0300

    cleaning

--- renderer.go
+++ renderer.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 var maze = [][]int{
@@ -16,32 +15,26 @@ var maze = [][]int{
 	{1, 1, 1, 1, 1, 1, 1},
 }
 
-var playerX, playerY int = 1, 5
+var playerX, playerY = 1, 5
 
 func printMaze() {
 	for y, row := range maze {
 		for x, col := range row {
-			if playerX == x && playerY == y {
+			switch {
+			case playerX == x && playerY == y:
 				fmt.Print("P ")
-			} else {
-				switch col {
-				case 0:
-					fmt.Print("  ")
-				case 1:
-					fmt.Print("█ ")
-				case 2:
-					fmt.Print("E ")
-				}
+			case col == 0:
+				fmt.Print("  ")
+			case col == 1:
+				fmt.Print("█ ")
+			case col == 2:
+				fmt.Print("E ")
 			}
 		}
 		fmt.Println()
 	}
 }
 
-func setupEnv() {
-}
-
 func main() {
-	setupEnv()
 	printMaze()
 }
