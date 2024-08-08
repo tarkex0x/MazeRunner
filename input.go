ff201fddbd83985cd26d228ffa997dc52571e61e
tarkex0x <163349630+tarkex0x@users.noreply.github.com>
2024-08-08 08:55:07 +0300

    clean code

--- input.go
+++ input.go
@@ -36,32 +36,33 @@ func readUserInput(prompt string) (string, error) {
 	fmt.Print(prompt)
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
-	if err != nil {
-		return "", err // Return the error to be handled by the caller
+	return strings.TrimSpace(input), err
+}
+
+func processCommand(command Command) {
+	switch command {
+	case MoveUp:
+		fmt.Println("Moving up.")
+	case MoveDown:
+		fmt.Println("Moving down.")
+	case MoveLeft:
+		fmt.Println("Moving left.")
+	case MoveRight:
+		fmt.Println("Moving right.")
+	default:
+		fmt.Println("Invalid command, please enter W, A, S, or D.")
 	}
-	return strings.TrimSpace(input), nil
 }
 
 func main() {
 	for {
 		userInput, err := readUserInput("Enter move (W/A/S/D): ")
 		if err != nil {
-			fmt.Println("Error reading input:", err) //Handling error here
+			fmt.Println("Error reading input:", err)
 			continue
 		}
 
 		command := convertInputToCommand(userInput)
-		switch command {
-		case MoveUp:
-			fmt.Println("Moving up.")
-		case MoveDown:
-			fmt.Println("Moving down.")
-		case MoveLeft:
-			fmt.Println("Moving left.")
-		case MoveRight:
-			fmt.Println("Moving right.")
-		case Invalid:
-			fmt.Println("Invalid command, please enter W, A, S, or D.")
-		}
+		processCommand(command)
 	}
 }
